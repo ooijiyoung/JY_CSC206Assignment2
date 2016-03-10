9dfd00a07117d607862badfee8b36192f5719077
Ooi Ji Young <ooijiyoung@gmail.com>
2016-03-10 22:15:30 +0800

    add Stock class

---
+++ Assignment2/Assignment2/Stock.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "Stock.h"
+using namespace std;
+
+//IMPOEKOEPOTAJIPOEJIPO ijofijeio jioGirbnjoeg
+
+//setter
+void Stock::setSymbol(string sim) {	symbol = sim;}
+void Stock::setPrice(double p) { price = p; }
+void Stock::setShares(int s) { shares = s; }
+void Stock::setOpening(double o) {oPrice = o;}
+void Stock::setClosing(double c) { cPrice = c; }
+void Stock::setHighPrice(double h) { hPrice = h; }
+void Stock::setLowPrice(double l) { lPrice = l; }
+void Stock::setPrevPrice(double pp) { pPrice = pp; }
+void Stock::setPercent(double per) { percent = per; }
+//getter
+string Stock::getSymbol() { return symbol; }
+double Stock::getPrice() { return price; }
+int Stock::getShares() { return shares; }
+double Stock::getOpening() { return oPrice; }
+double Stock::getClosing() { return cPrice; }
+double Stock::getHighPrice() { return hPrice; }
+double Stock::getLowPrice() { return lPrice; }
+double Stock::getPrevPrice() { return pPrice; }
+double Stock::getPercent() { return percent; }
---
+++ Assignment2/Assignment2/Stock.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+using namespace std;
+#ifndef _STOCK_
+#define _STOCK_
+
+class Stock {
+private:
+	string symbol;
+	double price; //stock Price
+	int shares; // number of shares
+	double oPrice; //opening price
+	double cPrice; //closing price
+	double hPrice; //high price
+	double lPrice; //low price
+	double pPrice; //previous price
+	double percent; //percent gain/loss
+public:
+	//setter
+	void setSymbol(string sim);
+	void setPrice(double p);
+	void setShares(int s);
+	void setOpening(double o);
+	void setClosing(double c);
+	void setHighPrice(double h);
+	void setLowPrice(double l);
+	void setPrevPrice(double pp);
+	void setPercent(double per);
+	//getter
+	string getSymbol();
+	double getPrice();
+	int getShares();
+	double getOpening();
+	double getClosing();
+	double getHighPrice();
+	double getLowPrice();
+	double getPrevPrice();
+	double getPercent();
+
+};
+
+
+#endif // !_STOCK_
