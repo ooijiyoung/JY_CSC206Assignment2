18f5350e21144cede2c6344fb4bc1ffadf47f30d
Ooi Ji Young <ooijiyoung@gmail.com>
2016-03-10 23:18:21 +0800

    Nothing - Test Commit

--- Assignment2/Assignment2/main.cpp
+++ Assignment2/Assignment2/main.cpp
@@ -2,6 +2,17 @@
 #include <string>
 #include "JiYoung.h"
 using namespace std;
+//just making sure they don't fluctuate
+const int ONE = 1;
+const int TWO = 2;
+const int THREE = 3;
+const int FOUR = 4;
+const int FIVE = 5;
+const int SIX = 6;
+const int SEVEN = 7;
+const int EIGHT = 8;
+const int NINE = 9;
+const int TEN = 10;
 
 int main(void) {
 	JiYoung ojy;
