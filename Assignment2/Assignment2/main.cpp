14c129f3c3967f5ae5b04e0f754ed97af9d2e76b
Ooi Ji Young <ooijiyoung@gmail.com>
2016-03-18 07:50:37 +0800

    unstupidify

--- Assignment2/Assignment2/main.cpp
+++ Assignment2/Assignment2/main.cpp
@@ -7,17 +7,6 @@
 
 using namespace std;
 
-//just making sure they don't fluctuate
-const int ONE = 1;
-const int TWO = 2;
-const int THREE = 3;
-const int FOUR = 4;
-const int FIVE = 5;
-const int SIX = 6;
-const int SEVEN = 7;
-const int EIGHT = 8;
-const int NINE = 9;
-const int TEN = 10;
 
 int main(void) {
 	JiYoung ojy;
