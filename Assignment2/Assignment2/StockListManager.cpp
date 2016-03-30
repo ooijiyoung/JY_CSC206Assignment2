e507ac66a4de0b909b2dc04901d7d5cd7c1f9fe4
Ooi Ji Young <ooijiyoung@gmail.com>
2016-03-30 15:16:18 +0800

    Added

---
+++ Assignment2/Assignment2/StockListManager.cpp
@@ -0,0 +1,4 @@
+#include "Stock.h"
+#include "StockListManager.h"
+#include <iostream>
+#include "JiYoung.h"
---
+++ Assignment2/Assignment2/StockListType.cpp
@@ -0,0 +1,4 @@
+#include "StockListType.h"
+#include "StockListManager.h"
+#include <iostream>
+#include "JiYoung.h"
