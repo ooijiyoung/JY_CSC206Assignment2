de16d1497676e6f5dcf9b86e48e1ce3dad663efa
Ooi Ji Young <ooijiyoung@gmail.com>
2016-03-30 14:42:34 +0800

    (╯°□°）╯︵ ┻━┻

---
+++ Assignment2/Assignment2/StockListType.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Stock.h"
+#include "JiYoung.h"
+
+using namespace std;
+
+#ifndef _StockListType_
+#define _StockListType_
+
+class StockListType {
+private:
+	Stock *stock;
+	int stockSize;
+public:
+
+};
+
+
+
+#endif // !_StockListType_
